df7e35366f6ee3242c921c981ad6d68c899369b5
gustavo-5G <131722213+gustavo-5G@users.noreply.github.com>
2023-12-24 13:24:00 -0300

    Add files via upload
    ficou meio simples... até tinha mais ideias mas por conta do bug que ocorria no Java script recorri a simplificar o código para evitar problemas acabando infelizmente limitando o projeto nesse sentido, além de não de eu não conseguir obter outra forma melhor de comparar as IAs como o uso direto delas, contudo esse foi o resultado o qual honestamente não achei satisfatório em geral mas foi o quê foi possível realizar na minha margem limitada de conhecimento de programação atual...

---
+++ codigoFake.js
@@ -0,0 +1,18 @@
+function salaSecreta() {
+    window.location = "SalaSecreta.html"
+}
+
+function codigoCentral() {
+    window.location = "main.js"
+}
+
+// Código iniciado...
+
+document.getElementById("CentralCommander").innerHTML = "Function == true"
+document.getElementById("CentralCommander").innerHTML = "Function == true"
+document.getElementById("CentralCommander").innerHTML = "Function == true"
+
+// Achou que ficaria assim ?!
+// HAHAHAHAHA!
+
+// Boa sorte para sair agora... 
---
+++ main.js
@@ -0,0 +1,10 @@
+
+function codigoCentral() {
+    window.location = "SalaSecreta.html"
+}
+function fakeJavascript() {
+    window.location = "codigoFake.js"
+}
+function sair() {
+    fakeJavascript()
+}
